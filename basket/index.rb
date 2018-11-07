05d5271433cd789117f1fc5e38bc2d36e8e39d69
YerassylAitkazy <yerassyl.ad@gmail.com>
2018-11-07 20:15:02 +0600

    basket 2.0

--- basket/index.rb
+++ basket/index.rb
@@ -1,31 +1,30 @@
-basketHash = {}
+basket_hash = {}
 
 loop do
   puts "Введите НАЗВАНИЕ товара"
   name = gets.chomp.downcase
   break if  name == "стоп"
-  basketHash[name] = {}
 
   puts "Введите ЦЕНУ за единицу товара"
   price = gets.chomp.to_f
-  basketHash[name]["price"] = price
 
   puts "Введите КОЛИЧЕСТВО товара"
   amount = gets.chomp.to_f
-  basketHash[name]["amount"] = amount
+
+  basket_hash[name.to_sym] = {price: price, amount: amount}
 end
 
 # print hash of items
-print "\n\nХэш товаров: ", basketHash, "\n\n"
+puts "Хэш товаров: #{basket_hash}"
 
-arrOfPrices = []
+arr_of_prices = []
 
 # print total price for each item
-basketHash.each do |key, val|
-  totalItem = val["price"] * val["amount"]
-  arrOfPrices.push(totalItem)
+basket_hash.each do |key, val|
+  totalItem = val[:price] * val[:amount]
+  arr_of_prices.push(totalItem)
   puts "#{key}: #{totalItem}"
 end
 
 # total price for ALL items
-print "\nИтоговая сумма равна: ", arrOfPrices.sum, "\n"
+puts "Итоговая сумма #{arr_of_prices.sum}"
